22c435b9cb2bf14097fcda43714e8684efa99ca7
Rahil Vora <rahil.vora740@gmail.com>
2015-07-23 20:20:54 +0530

    Added static root

--- zaya/settings.py
+++ zaya/settings.py
@@ -100,5 +100,5 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
